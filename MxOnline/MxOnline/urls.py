fdc31a67eea437407166997057accc533dc48c98
YanWeiXing <279696743@qq.com>
2018-02-20 23:00:43 +0800

    complete base operation functions of users

--- MxOnline/MxOnline/urls.py
+++ MxOnline/MxOnline/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import xadmin
 from django.views.generic import TemplateView
 
-from users.views import LoginView, LogoutView
+from users.views import (LoginView, LogoutView, RegisterView, ActiveCodeView,
+                         ForgetPWDView, ResetCodeView, ModifyPwdView)
 
 
 urlpatterns = [
@@ -26,5 +27,12 @@ urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('captcha/', include('captcha.urls')),
+    path('active/<active_code>', ActiveCodeView.as_view(), name='user_active'),
+    path('forget/', ForgetPWDView.as_view(), name='forget_pwd'),
+    path('reset/<reset_code>', ResetCodeView.as_view(), name='reset_pwd'),
+    path('modify_pwd/', ModifyPwdView.as_view(), name='modify_pwd'),
+
 ]
